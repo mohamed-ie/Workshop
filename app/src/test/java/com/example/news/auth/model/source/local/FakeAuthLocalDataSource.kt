dbf424ba1a84aaec26fc3094dcb67eb155984773
Rafeef <rafeefalmahy@gmail.com>
2023-05-26 12:20:06 +0300

    add fake local and remote data for test - add fakeAuthLocalDataSource - add fake authWebservice

---
+++ app/src/test/java/com/example/news/auth/model/source/local/FakeAuthLocalDataSource.kt
@@ -0,0 +1,12 @@
+package com.example.news.auth.model.source.local
+
+class FakeAuthLocalDataSource : AuthLocalDataSource{
+    override fun saveUserData(userID: Int, name: String, email: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun resetUserData() {
+        TODO("Not yet implemented")
+    }
+
+}
