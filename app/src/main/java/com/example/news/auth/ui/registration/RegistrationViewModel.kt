592366e9395f52260929751d9c3fff33f60616e3
hamed <hamed.arfat@icloud.com>
2023-05-26 12:43:41 +0300

    - Remove dumpy code

--- app/src/main/java/com/example/news/auth/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/news/auth/ui/registration/RegistrationViewModel.kt
@@ -6,21 +6,4 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class RegistrationViewModel:ViewModel() {
 
-    private val _error = MutableStateFlow("")
-    val error = _error.asStateFlow()
-
-    private val _userName = MutableStateFlow("")
-    val userName = _userName.asStateFlow()
-
-    private val _email = MutableStateFlow("")
-    val email = _email.asStateFlow()
-
-    private val _password = MutableStateFlow("")
-    val password = _password.asStateFlow()
-
-
-
-
-
-
 }
