dacfdcba46ff501e18be96c5a8896e7dab525733
mohammed-ie <mohammedie98@gmail.com>
2023-05-27 04:13:56 +0300

    add registrationViewModel test

--- app/src/test/java/com/example/news/auth/model/source/remote/FakeAuthWebservice.kt
+++ app/src/test/java/com/example/news/auth/model/source/remote/FakeAuthWebservice.kt
@@ -1,12 +1,19 @@
 package com.example.news.auth.model.source.remote
 
+import com.example.news.auth.model.source.remote.body.SignupBody
+import com.example.news.auth.model.source.remote.dto.AuthDto
+
 
 class FakeAuthWebservice :AuthWebservice{
-    override suspend fun login(email: String, password: String) {
+    override suspend fun login(
+        email: String,
+        password: String,
+        returnSecureToken: Boolean
+    ): AuthDto {
         TODO("Not yet implemented")
     }
 
-    override suspend fun signup(email: String, password: String, displayName: String) {
+    override suspend fun signup(signupBody: SignupBody): AuthDto {
         TODO("Not yet implemented")
     }
 
