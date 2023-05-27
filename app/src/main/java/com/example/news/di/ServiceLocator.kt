815383545018de2f93029474b6833d22428ace2d
Rafeef <rafeefalmahy@gmail.com>
2023-05-27 15:17:35 +0300

    handel internetConnection

--- app/src/main/java/com/example/news/di/ServiceLocator.kt
+++ app/src/main/java/com/example/news/di/ServiceLocator.kt
@@ -1,7 +1,9 @@
 package com.example.news.di
 
+import android.net.ConnectivityManager
 import com.example.news.auth.model.repository.AuthRepository
 
 interface ServiceLocator {
     val authRepository: AuthRepository
+    val connectivityManager : ConnectivityManager
 }
--- app/src/main/java/com/example/news/di/ServiceLocatorImpl.kt
+++ app/src/main/java/com/example/news/di/ServiceLocatorImpl.kt
@@ -1,25 +1,17 @@
 package com.example.news.di
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.preferences.SharedPreferencesMigration
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.emptyPreferences
+import android.net.ConnectivityManager
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.datastore.preferences.preferencesDataStoreFile
 import com.example.news.BuildConfig
 import com.example.news.auth.model.repository.AuthRepository
 import com.example.news.auth.model.repository.AuthRepositoryImpl
 import com.example.news.auth.model.source.local.AuthLocalDataSourceImpl
 import com.example.news.auth.model.source.local.UserDataStoreManagerImpl
 import com.example.news.auth.model.source.remote.AuthRemoteDataSourceImpl
-import com.example.news.auth.model.source.remote.interceptor.AuthInterceptor
 import com.example.news.auth.model.source.remote.AuthWebservice
-import kotlinx.coroutines.CoroutineScope
+import com.example.news.auth.model.source.remote.interceptor.AuthInterceptor
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -29,8 +21,7 @@ class ServiceLocatorImpl(private val context: Context) : ServiceLocator {
     companion object {
         const val SHARED_PREFERENCES_NAME = "USER"
     }
-
-    private val retrofitBuilder by lazy {
+        private val retrofitBuilder by lazy {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
     }
@@ -57,7 +48,7 @@ class ServiceLocatorImpl(private val context: Context) : ServiceLocator {
         val localDataSource = AuthLocalDataSourceImpl(dataStoreManager = UserDataStoreManagerImpl(context.dataStore))
         AuthRepositoryImpl(remoteDataSource,localDataSource,Dispatchers.Default)
     }
-
-
+    override val connectivityManager: ConnectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
 }
