116c8fbb89d55d84c0498866cb75d271f7df6f2a
hamed <hamed.arfat@icloud.com>
2023-05-27 12:08:40 +0300

    - Add news remote data source and it's interface - update news repository and it's interface

--- app/src/main/java/com/example/news/news/model/repository/NewsRepository.kt
+++ app/src/main/java/com/example/news/news/model/repository/NewsRepository.kt
@@ -1,4 +1,8 @@
 package com.example.news.news.model.repository
 
+import com.example.news.news.model.source.remote.dto.NewsDTO
+import kotlinx.coroutines.flow.Flow
+
 interface NewsRepository {
+    fun getNews(country:String):Flow<NewsDTO>
 }
--- app/src/main/java/com/example/news/news/model/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/news/news/model/repository/NewsRepositoryImpl.kt
@@ -1,4 +1,18 @@
 package com.example.news.news.model.repository
 
-class NewsRepositoryImpl : NewsRepository {
+import com.example.news.news.model.source.remote.NewsRemoteDataSource
+import com.example.news.news.model.source.remote.dto.NewsDTO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class NewsRepositoryImpl(
+    private val remoteDataSource: NewsRemoteDataSource
+) : NewsRepository {
+    override fun getNews(country:String): Flow<NewsDTO> = flow {
+        emit(remoteDataSource.newsHeadLines(country))
+    }
+
+
+
+
 }
