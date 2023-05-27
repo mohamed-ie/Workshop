a183f348662226efc9f3dda3a774d27026d50ee9
Rafeef <rafeefalmahy@gmail.com>
2023-05-27 16:18:46 +0300

    add newsRepo and connectivityManager imports

--- app/src/main/java/com/example/news/di/ServiceLocator.kt
+++ app/src/main/java/com/example/news/di/ServiceLocator.kt
@@ -1,6 +1,8 @@
 package com.example.news.di
 
+import android.net.ConnectivityManager
 import com.example.news.auth.model.repository.AuthRepository
+import com.example.news.news.model.repository.NewsRepository
 
 interface ServiceLocator {
     val authRepository: AuthRepository
